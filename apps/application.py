e54549e8e9d6d06aa6ab16bb285b04110e0615e1
MayconMoreira <73704595+MayconMoreira@users.noreply.github.com>
2021-06-29 16:00:10 -0300

    Update application.py

--- apps/application.py
+++ apps/application.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:maycon123@pokeapi-aws.crdwswc1sphu.us-east-2.rds.amazonaws.com/pokeapi'
-app.config['SECRET_KEY'] = 'secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv("URI_MYSQL")
+app.config['SECRET_KEY'] = getenv("SECRET_KEY")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
